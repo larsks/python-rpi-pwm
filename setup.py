f26c082c6a2629daf561529c8db051fe8cff7506
Lars Kellogg-Stedman <lars@oddbit.com>
2017-09-30 11:21:25 -0400

    prep for packaging
    added setup.py and README

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rpi-pwm',
+    version='0.1',
+    author='Lars Kellogg-Stedman',
+    author_email='lars@oddbit.com',
+    description='Support for Raspberry Pi hardware PWM',
+    license='GPLv3',
+    url='https://github.com/larsks/python-rpi-pwm',
+    packages=find_packages(),
+)
